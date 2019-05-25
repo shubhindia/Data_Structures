5aadb132e78dfad8bbd09d04981b9c2f7a467771
shubhindia <shubhindia123@gmail.com>
2019-05-25 13:20:02 +0530

    add insertAfter function and fix some typos
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- linked_list/push.cpp
+++ linked_list/push.cpp
@@ -21,14 +21,34 @@ void display(Node *n)
     n = n->next;
   }
 }
-//function to inset data into new node
+//function to insert data into new node
 void push(Node **head_ref, int new_data)
 {
   Node *new_node = new Node();
   new_node->data= new_data;
   new_node->next = (*head_ref);
   (*head_ref)= new_node;
+}
 
+//function to insert the node at the given point
+
+void insertAfter(Node *prev_node, int new_data)
+{
+  if(prev_node== NULL)
+  {
+    cout<<"Previous node cann not be null";
+    return;
+  }
+  //create a new node
+  Node *new_node = new Node();
+  //insert data into new Node
+  new_node->data= new_data;
+  //point the new node's next to the next node (in our case it will be previous
+  //element's next node because we are inseting after that previout node)
+  new_node->next = prev_node->next;
+  //point previous node's next to our new node so that it will become part of
+  //the linked list
+  prev_node->next= new_node;
 }
 
 int main()
@@ -37,6 +57,10 @@ int main()
 Node *head = NULL;
 push(&head, 1);
 push(&head, 2);
+insertAfter(head->next,3);
+insertAfter(head->next,4);
+insertAfter(head->next->next,5);
+insertAfter(head->next,6);
 display(head);
   return 0;
 
