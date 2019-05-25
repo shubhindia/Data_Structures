0386ab621d09177fb2848a05db97079ea1dcad82
shubhindia <shubhindia123@gmail.com>
2019-05-25 15:16:48 +0530

    stack: count the number of elements in stack
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

---
+++ stack/count.cpp
@@ -0,0 +1,23 @@
+#include<stack>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int count=0;
+stack<int> mystack;
+
+mystack.push(1);
+mystack.push(2);
+mystack.push(3);
+mystack.push(4);
+mystack.push(5);
+
+while(!mystack.empty())
+{
+  count++;
+  mystack.pop();
+}
+cout<<count<<"\n";
+
+}
