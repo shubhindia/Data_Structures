abc290d4c57a038074df17d0f6c1796b1ce90cfc
shubhindia <shubhindia123@gmail.com>
2019-05-25 12:35:42 +0530

    Node insertion
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- linked_list/insert.cpp
+++ linked_list/insert.cpp
@@ -1,10 +1,42 @@
-#include<stdio.h>
-#include<iosteam>
+#include<bits/stdc++.h>
 #inlcude<stdlib.h>
 using namespace std;
 
-//lined list node
-struct Node
+//linked list node
+class Node
 {
+int n;
+int data;
+Node *next;
+};
+
+//driver to insert 3 nodes to linked list
+int main()
+{
+
+//delcare 3 nodes of the linked list
+Node *head=NULL;
+Node *second=NULL;
+Node *third=NULL;
+
+// add nodes in heap 
+head = new Node();
+second = new Node();
+third = new Node();
+
+// insert data and create link
+
+head->data=1;
+head->next = second;
+
+second->data=2;
+second->next  = third;
+
+third->data = 3;
+third->next = NULL; // last node of the linked list
+
+
+return 0;
+
 }
 
